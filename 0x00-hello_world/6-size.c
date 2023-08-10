0667de62e134764f4c6f45352238e38f57ca8d42
Sakina1995 <sakinatarig@gmail.com>
2023-08-10 13:35:22 -0700

    task 6-alx-low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*description: A program that prints the size of various types
+*main - A program that prints the size of various types
 *Return: 0 (Success)
 */
 int main(void)
 {
-printf("Size of a char: %u byte(s)\n", sizeof(char));
-printf("Size of an int: %u byte(s)\n", sizeof(int));
-printf("Size of a long: %u byte(s)\n", sizeof(long));
-printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
-printf("Size of a float: %u byte(s)\n", sizeof(float));
+char A;
+int B;
+long int C;
+long long int D;
+float F;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(A));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(B));
+printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(C));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(D));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(F));
 return (0);
 }
