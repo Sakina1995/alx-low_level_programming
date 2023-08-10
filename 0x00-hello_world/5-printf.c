4a1606b897245bc16ee2d95e050a5f579db73ab6
Sakina1995 <sakinatarig@gmail.com>
2023-08-10 12:17:48 -0700

    task 5-alx-low_level_programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - A c program that prints with printf function
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+Printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
