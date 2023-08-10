8fde0acb4333ff83c6f6bb58701d17694aebfa34
Sakina1995 <sakinatarig@gmail.com>
2023-08-10 12:02:45 -0700

    task 4-alx-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - A c program that prints with put function
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
