5cd32e7c0778ac851cbddcbc4d434c7e7ebc08e3
Sakina1995 <sakinatarig@gmail.com>
2023-08-11 16:53:41 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c, y;
+for (c = '0' ; c <= 98 ; c++)
+{
+for (y = c + 1 ; y <= 99 ; y++)
+{
+putchar((c / 10) + '0');
+putchar((c % 10) + '0');
+putchar(' ');
+putchar((y / 10) + '0');
+putchar((y % 10) + '0');
+if (c == 98 && y == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
