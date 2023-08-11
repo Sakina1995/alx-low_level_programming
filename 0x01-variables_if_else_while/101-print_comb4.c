0aab077bf1e2404e891ab1c94531331d8de5b8fa
Sakina1995 <sakinatarig@gmail.com>
2023-08-11 16:36:59 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,20 @@
 int main(void)
 {
 int c, y, z;
-for (c = '0' ; c < '9' ; c++)
+for (c = '0'; c < '9'; c++)
 {
-for (y = c + 1 ; y <= '9' ; y++)
+for (y = c + 1; y <= '9'; y++)
 {
-for (z = y + 1 ; z <= '9' ; z++)
+for (z = y + 1; z <= '9'; z++)
 {
-if ((c != y) != z)
+if ((y != c) != z)
 {
 putchar(c);
 putchar(y);
-putchar(y);
+putchar(z);
 if (c == '7' && y == '8')
 continue;
-putchar(',');
+putchar (',');
 putchar(' ');
 }
 }
