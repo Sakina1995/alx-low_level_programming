020a90f95cc14f4f77f34af211acbb2448703589
Sakina1995 <sakinatarig@gmail.com>
2023-08-15 06:44:37 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char (str[]) = "_putchar";
+int ch;
+for (ch = '0' ; c < '8' ; ch++)
+_putchar(str[ch]);
+putchar('\n');
+return (0);
+}
