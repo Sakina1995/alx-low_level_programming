bcd51050ac8a930a7c4854fce12e1be4f4f81dd9
Sakina1995 <sakinatarig@gmail.com>
2023-08-15 16:32:02 -0700

    task14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main- Prints the first 50 fibonacci numbers
+* Return: Nothing!
+*/
+int main(void)
+{
+long int i, j, k, next;
+j = 1;
+k = 2;
+for  (i = 1; i <= 50; ++i)
+{
+if (j  != 20365011074)
+{
+printf("%ld",  j);
+}
+else
+{
+printf("%ld\n", j);
+}
+next  = j + k;
+k = next;
+}
+return (0);
+}
